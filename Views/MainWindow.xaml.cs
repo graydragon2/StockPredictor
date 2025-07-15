585c5a4e9b61528c34909c97b3275f84b9c66dd6
graydragon2 <52837063+graydragon2@users.noreply.github.com>
2025-07-14 20:39:16 -0400

    Add files via upload

---
+++ Views/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows;
+
+namespace StockPredictor.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
