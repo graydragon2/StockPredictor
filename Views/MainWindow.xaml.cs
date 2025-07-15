86282ec1256106f1bced0e57b1c05ed2bfb55bd6
graydragon2 <52837063+graydragon2@users.noreply.github.com>
2025-07-14 20:43:42 -0400

    Update MainWindow.xaml.cs

--- Views/MainWindow.xaml.cs
+++ Views/MainWindow.xaml.cs
@@ -1,12 +1,21 @@
 using System.Windows;
+using StockPredictor.ViewModels;
 
 namespace StockPredictor.Views
 {
     public partial class MainWindow : Window
     {
+        private InvestmentViewModel _vm;
+
         public MainWindow()
         {
             InitializeComponent();
+            _vm = DataContext as InvestmentViewModel;
+        }
+
+        private void AddInvestment_Click(object sender, RoutedEventArgs e)
+        {
+            _vm?.AddInvestment();
         }
     }
 }
